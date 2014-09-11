893f0d0aa07fa12aa1e82947dced3a27705a6638
sebyd <seby_dum@yahoo.com>
2014-09-11 16:27:44 +0300

    Added CompletedTasks class to project, to store all completed tasks from a week ago and to the time of the report generating.

---
+++ JiraReporter/Model/CompletedTasks.cs
@@ -0,0 +1,14 @@
+﻿using JiraReporter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JiraReporter
+{
+    public class CompletedTasks
+    {
+        public List<Task> Tasks { get; set; }
+        public string CompletedTimeAgo { get; set; }
+    }
+}
