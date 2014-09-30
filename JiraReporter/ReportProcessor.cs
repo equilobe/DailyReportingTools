f1c1eaca6a522e19356e5f03dd48cc86b4a8a5ae
Sebastian Dumitrascu <seby_dum@yahoo.com>
2014-09-30 18:41:17 +0300

    - Updated references; - Updated template; - Added method to render partial view to string;

--- JiraReporter/ReportProcessor.cs
+++ JiraReporter/ReportProcessor.cs
@@ -1,20 +1,40 @@
 ﻿using JiraReporter.Model;
 using RazorEngine;
+using RazorEngine.Templating;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
 namespace JiraReporter
 {
     class ReportProcessor
     {
         public static string ProcessReport(Report report)
         {
-            string template = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\Views\TimesheetReportTemplate.cshtml");            
+            string template = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\Views\TimesheetReportTemplate.cshtml");
+          //  var partial = RenderPartialToString(@"Views\_partial.cshtml", report);
+         //   template += partial;
             return Razor.Parse(template, report);
         }
-    }
+
+        public static string RenderPartialToString(string viewPath, object model)
+        {
+            string viewAbsolutePath = MapPath(viewPath);
+
+            var viewSource = File.ReadAllText(viewAbsolutePath);
+
+            string renderedText = Razor.Parse(viewSource, model);
+            return renderedText;
+        }
+
+        public static string MapPath(string filePath)
+        {
+            return HttpContext.Current != null ? HttpContext.Current.Server.MapPath(filePath) : string.Format("{0}{1}", AppDomain.CurrentDomain.BaseDirectory, filePath.Replace("~", string.Empty).TrimStart('/'));
+        }    
+    }         
 }
