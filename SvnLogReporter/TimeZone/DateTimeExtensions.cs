af73eb592015cde2fa12eebaa903eecf7baf36f5
Sebastian Dumitrascu <seby_dum@yahoo.com>
2014-10-23 08:27:02 -0700

    DRT-26 set report dates - new class for setting the correct time zone : DateTimeExtensions.cs

---
+++ SvnLogReporter/TimeZone/DateTimeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class DateTimeExtensions
+    {
+
+        static DateTimeExtensions() {
+            OffsetFromUtc = TimeZoneInfo.Local.GetUtcOffset(DateTime.Now);
+        }
+
+       
+        public static TimeSpan OffsetFromUtc { get; set; }
+
+        public static DateTime ToOriginalTimeZone(this DateTime date)
+        {
+            return date.ToUniversalTime().Add(OffsetFromUtc);
+        }
+
+
+        public static void SetOriginalTimeZoneFromDateAtMidnight(DateTime dateAtMidnight)
+        {
+            var universalDate = dateAtMidnight.ToUniversalTime();
+
+            var referenceDay = universalDate.Date;
+
+            if(referenceDay.AddHours(12) < universalDate )
+                referenceDay = referenceDay.AddDays(1);
+
+            OffsetFromUtc = referenceDay - universalDate;
+        }
+    }
+}
