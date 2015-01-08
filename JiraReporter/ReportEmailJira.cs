d9f1b3b5da01fcb55e831ac568d8343eaef2bb49
Sebastian Dumitrascu <seby_dum@yahoo.com>
2015-01-08 15:22:27 +0200

    DRT-76 - implemented ReportEmailJira to inherit ReportEmailer; - add attachments to email with users avatars; - implement class, WebDownloads, used to download images; - updated summary partial class; - updated methods access in ReportEmailer

---
+++ JiraReporter/ReportEmailJira.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SourceControlLogReporter;
+using System.Drawing;
+using System.Net.Mail;
+using System.IO;
+using System.Drawing.Imaging;
+using JiraReporter.Model;
+using System.Net.Mime;
+
+namespace JiraReporter
+{
+    public class ReportEmailJira : ReportEmailer
+    {
+        public List<Author> Authors { get; set; }
+
+        public ReportEmailJira(SourceControlLogReporter.Model.Policy policy, SourceControlLogReporter.Options options) : base(policy, options)
+        {
+            this.policy = policy;
+            this.options = options;
+        }
+        public void AddAttachementImage(Image image, string id, MailMessage mailMessage)
+        {
+            MemoryStream stream = new MemoryStream();
+            image.Save(stream, ImageFormat.Png);
+            var imgBytes = stream.ToArray();
+
+            MemoryStream ms = new MemoryStream(imgBytes);
+            var attachment = new Attachment(ms, id, "image/png");
+            attachment.ContentDisposition.Inline = true;
+            attachment.ContentDisposition.DispositionType = DispositionTypeNames.Inline;
+            attachment.ContentId = id;
+            attachment.ContentType.Name = id;
+            mailMessage.Attachments.Add(attachment);
+        }
+
+        public override void EmailReport(string reportPath)
+        {
+            var smtp = new SmtpClient { EnableSsl = true };
+            var message = GetMessage(reportPath);
+
+            foreach (var author in Authors)
+            {
+                var image = WebDownloads.ImageFromURL(author.AvatarLink.OriginalString, policy.Username, policy.Password);
+                AddAttachementImage(image, author.Username, message);
+            }
+
+            smtp.Send(message);
+
+            MoveToSent(reportPath);
+            if (policy.IsDraft == false)
+                policy.WriteDateToPolicy(options.PolicyPath, DateTime.Now.ToOriginalTimeZone());
+        }
+    }
+}
---
+++ JiraReporter/WebDownloads.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraReporter
+{
+    public class WebDownloads
+    {
+        public static Image ImageFromURL(string Url, string username, string password)
+        {
+            byte[] imageData = DownloadData(Url, username, password);
+
+            MemoryStream stream = new MemoryStream(imageData);
+            var img = Image.FromStream(stream);
+            stream.Close();
+
+
+            return img;
+        }
+
+        private static byte[] DownloadData(string Url, string username, string password)
+        {
+            byte[] downloadedData = new byte[0];
+            try
+            {
+                WebRequest req = WebRequest.Create(Url);
+                req.Credentials = new NetworkCredential(username, password);
+                WebResponse response = req.GetResponse();
+                Stream stream = response.GetResponseStream();
+
+                byte[] buffer = new byte[1024];
+
+                int dataLength = (int)response.ContentLength;
+
+                MemoryStream memStream = new MemoryStream();
+                while (true)
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        memStream.Write(buffer, 0, bytesRead);
+                    }
+                }
+
+                downloadedData = memStream.ToArray();
+
+                stream.Close();
+                memStream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+            return downloadedData;
+        }
+    }
+}
