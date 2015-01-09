169fc01ce860ffa5b0276626c625235eb9eeea38
DanBadea <dan.badea@equilobe.com>
2015-01-09 14:00:24 +0200

    refactored ReportController actions

--- DailyReportWeb/Controllers/ReportController.cs
+++ DailyReportWeb/Controllers/ReportController.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.Win32.TaskScheduler;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DailyReportWeb.Helpers;
 
 namespace DailyReportWeb.Controllers
 {
@@ -13,54 +13,25 @@ namespace DailyReportWeb.Controllers
         [HttpGet]
         public ActionResult Send(string id, DateTime date)
         {
-            if (date.Date == DateTime.Today)
-            {
-                var sendReport = RunReport(id);
-                if (sendReport == true)
-                    return Content("Report confirmed. Final report sent");
+            if (date.Date != DateTime.Today)
+                return Content("Cannot confirm report for another date");
 
+            if (ReportRunner.TryRunReport(id))
+                return Content("Report confirmed. Final report sent");
+            else 
                 return Content("Error in sending the final report");
-            }
-
-            return Content("Cannot confirm report for another date");
         }
 
         [HttpGet]
         public ActionResult ResendDraft(string id, DateTime date)
         {
-            if (date.Date == DateTime.Today)
-            {
-                var resendReport = RunReport(id);
-                if (resendReport == true)
-                    return Content("Draft report was resent");
+            if (date.Date != DateTime.Today)
+                return Content("Cannot resend draft for another date");
 
+            if (ReportRunner.TryRunReport(id + "draft"))
+                return Content("Draft report was resent");
+            else 
                 return Content("Error in resending draft report");
-            }
-
-            return Content("Cannot resend draft for another date");
-        }
-
-        public bool RunReport(string id)
-        {
-            var key = "DRT-" + id;
-            using (TaskService ts = new TaskService())
-            {
-                var task = ts.AllTasks.ToList().Find(t => t.Name == key);
-                if (task != null)
-                {
-                    try
-                    {
-                        task.Run();
-                        return true;
-                    }
-                    catch (Exception ex)
-                    {
-                        return false;
-                    }
-                }
-                return false;
-                // return string.Join(", ", ts.AllTasks.Select(t => t.Name));
-            }
-        }
+        }     
     }
 }
---
+++ DailyReportWeb/Helpers/ReportRunner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Win32.TaskScheduler;
+
+namespace DailyReportWeb.Helpers
+{
+    public class ReportRunner
+    {
+        public static bool TryRunReport(string id)
+        {
+            try
+            {
+                RunReport(id);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static void RunReport(string id)
+        {
+            var key = "DRT-" + id;
+            using (var ts = new TaskService())
+            {
+                var task = ts.AllTasks.Single(t => t.Name == key);
+                task.Run();
+            }
+        }
+    }
+}
