5688c1658916e461ce95ba2ae4128d0f71c6a5ae
DanBadea <dan.badea@equilobe.com>
2015-01-20 14:57:57 +0200

    added angular.js package added a new api controller for working with policies created the root of the angular app and an initial js file

---
+++ DailyReportWeb/Controllers/Api/PolicyController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DailyReportWeb.Models;
+using SourceControlLogReporter.Model;
+
+namespace DailyReportWeb.Controllers.Api
+{
+    public class PolicyController : ApiController
+    {
+        // GET: api/Policy
+        public IEnumerable<PolicySummary> Get()
+        {
+            return new PolicySummary[] 
+            {
+                new PolicySummary { Id= "asdf1", ProjectName="Alyssa", ReportTime = "09:00 AM" },
+                new PolicySummary { Id= "asdf2", ProjectName="Board Prospects" },
+                new PolicySummary { Id= "asdf3", ProjectName="Daily Report Tool ", ReportTime = "09:00 AM" },
+            };
+        }
+
+        // GET: api/Policy/5
+        public Policy Get(string id)
+        {
+            return new Policy();
+        }
+
+        // Policies are not created directly!
+        // POST: api/Policy
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        // PUT: api/Policy/5
+        public void Put(string id, [FromBody]Policy updatedPolicy)
+        {
+            // TODO: save the updated policy
+        }
+
+        // Policies are not deleted directly!
+        // DELETE: api/Policy/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ DailyReportWeb/app/js/policyEditorApp.js
@@ -0,0 +1,6 @@
+﻿angular.module("policyEditorApp", [])
+    .controller("policyListPage", function ($scope, $http) {
+        $http.get("api/policy").done(function(result){
+            $scope.policyList = result;
+        })        
+    });
