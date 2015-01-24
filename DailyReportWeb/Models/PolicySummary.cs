d96cc5c098dfc06c81dd6525e8faa40e45069532
Vlad Ichimescu <vladichimescu@gmail.com>
2015-01-24 18:44:56 +0200

    added default value for ReportTime

--- DailyReportWeb/Models/PolicySummary.cs
+++ DailyReportWeb/Models/PolicySummary.cs
@@ -7,6 +7,11 @@ namespace DailyReportWeb.Models
 {
     public class PolicySummary
     {
+        public PolicySummary()
+        {
+            ReportTime = "09:00";
+        }
+        
         public string Id { get; set; }
         public string ProjectName { get; set; }
         public string ReportTime { get; set; } 
