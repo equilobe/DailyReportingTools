b8df4f0720045eb10dec7dee62b930ad0b416a1d
Vlad Ichimescu <vladichimescu@gmail.com>
2015-01-26 16:16:30 +0200

    Removed default ReportTime

--- DailyReportWeb/Models/PolicySummary.cs
+++ DailyReportWeb/Models/PolicySummary.cs
@@ -7,11 +7,6 @@ namespace DailyReportWeb.Models
 {
     public class PolicySummary
     {
-        public PolicySummary()
-        {
-            ReportTime = "09:00";
-        }
-        
         public string Id { get; set; }
         public string ProjectName { get; set; }
         public string ReportTime { get; set; } 
