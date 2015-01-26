50f72d130a6ff699d8f1e07a0c86e03736253548
Sebastian Dumitrascu <seby_dum@yahoo.com>
2015-01-26 15:27:52 +0200

    DRT-56 - implemented class IndividualReportInfoService.cs;

---
+++ JiraReporter/IndividualReportInfoService.cs
@@ -0,0 +1,54 @@
+﻿using JiraReporter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraReporter
+{
+    class IndividualReportInfoService
+    {
+        public void GetIndividualDraftInfo(List<Author> authors, SourceControlLogReporter.Model.Policy policy)
+        {
+            if(!policy.AdvancedOptions.NoIndividualDraft)
+            {
+                var individualDrafts = new List<SourceControlLogReporter.Model.IndividualDraftInfo>();
+                foreach(var author in authors)
+                {
+                    var individualDraft = GenerateIndividualDraftInfo(author, policy);
+                    individualDrafts.Add(individualDraft);
+                    author.IndividualDraftInfo = individualDraft;
+                }
+
+                policy.GeneratedProperties.IndividualDrafts = individualDrafts;
+            }
+        }
+
+        private SourceControlLogReporter.Model.IndividualDraftInfo GenerateIndividualDraftInfo(Author author, SourceControlLogReporter.Model.Policy policy)
+        {
+            var individualDraft = new SourceControlLogReporter.Model.IndividualDraftInfo
+            {
+                Name = author.Name,
+                Username = author.Username,
+                UserKey = RandomGenerator.RandomString(10)
+            };
+            SetIndividualUrls(individualDraft, policy);
+
+            return individualDraft;
+        }
+
+        private void SetIndividualUrls(SourceControlLogReporter.Model.IndividualDraftInfo individualDraft, SourceControlLogReporter.Model.Policy policy)
+        {
+            individualDraft.ConfirmationDraftUrl = SetUrl(individualDraft, policy.IndividualDraftConfirmationUrl);
+            individualDraft.ResendDraftUrl = SetUrl(individualDraft, policy.ResendIndividualDraft);
+        }
+
+        private static Uri SetUrl(SourceControlLogReporter.Model.IndividualDraftInfo individualDraft, Uri baseUrl)
+        {
+            var url = string.Format("draftKey={0}", individualDraft.UserKey);
+
+            return new Uri(baseUrl + "&" + url);
+        }
+    }
+}
