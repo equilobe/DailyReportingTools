56c49ecdaae6061b4122b03054fdc3a849fb66ad
Vlad Ichimescu <vladichimescu@gmail.com>
2015-01-28 10:43:45 +0200

    Removed leading slash '/' in rest api strings due to recommandation of atlassian-devs

--- JiraReporter/ApiUrls.cs
+++ JiraReporter/ApiUrls.cs
@@ -10,85 +10,85 @@ namespace JiraReporter
     {
         public static string Project(string id)
         {
-            return string.Format("/rest/api/2/project/{0}", id);
+            return string.Format("rest/api/2/project/{0}", id);
         }
         public static string Timesheet(string targetUser, string fromDate, string toDate)
         {
-            return string.Format("/rest/timesheet-gadget/1.0/raw-timesheet.xml?targetUser={0}&startDate={1}&endDate={2}", targetUser, fromDate, toDate);
+            return string.Format("rest/timesheet-gadget/1.0/raw-timesheet.xml?targetUser={0}&startDate={1}&endDate={2}", targetUser, fromDate, toDate);
         }
 
         public static string User(string userName)
         {
-            return string.Format("/rest/api/latest/user?username={0}", userName);
+            return string.Format("rest/api/latest/user?username={0}", userName);
         }
 
         public static string Users(string project)
         {
-            return string.Format("/rest/api/2/user/assignable/search?project={0}", project);
+            return string.Format("rest/api/2/user/assignable/search?project={0}", project);
         }
 
         public static string RapidView(string id)
         {
-            return string.Format("/rest/greenhopper/1.0/xboard/work/allData/?rapidViewId={0}", id);
+            return string.Format("rest/greenhopper/1.0/xboard/work/allData/?rapidViewId={0}", id);
         }
 
         public static string RapidViews()
         {
-            return "/rest/greenhopper/1.0/rapidviews/list";
+            return "rest/greenhopper/1.0/rapidviews/list";
         }
 
         public static string Sprint(string rapidViewId, string sprintId)
         {
-            return string.Format("/rest/greenhopper/1.0/rapid/charts/sprintreport?rapidViewId={0}&sprintId={1}", rapidViewId, sprintId);
+            return string.Format("rest/greenhopper/1.0/rapid/charts/sprintreport?rapidViewId={0}&sprintId={1}", rapidViewId, sprintId);
         }
 
         public static string AllSprints(string rapidViewId)
         {
-            return string.Format("/rest/greenhopper/1.0/sprintquery/{0}", rapidViewId);
+            return string.Format("rest/greenhopper/1.0/sprintquery/{0}", rapidViewId);
         }
         public static string IssueByKey(string issueKey)
         {
-            return string.Format("/rest/api/2/issue/{0}", issueKey);
+            return string.Format("rest/api/2/issue/{0}", issueKey);
         }
 
         public static string Issue()
         {
-            return "/rest/api/2/issue";
+            return "rest/api/2/issue";
         }
 
         public static string Priority()
         {
-            return "/rest/api/2/priority";
+            return "rest/api/2/priority";
         }
 
         public static string CreateMeta()
         {
-            return "/rest/api/2/issue/createmeta";
+            return "rest/api/2/issue/createmeta";
         }
 
         public static string Status()
         {
-            return "/rest/api/2/status";
+            return "rest/api/2/status";
         }
 
         public static string ApplicationProperties()
         {
-            return "/rest/api/2/application-properties";
+            return "rest/api/2/application-properties";
         }
 
         public static string AttachmentById(string attachmentId)
         {
-            return string.Format("/rest/api/2/attachment/{0}", attachmentId);
+            return string.Format("rest/api/2/attachment/{0}", attachmentId);
         }
 
         public static string Project()
         {
-            return "/rest/api/2/project";
+            return "rest/api/2/project";
         }
 
         public static string Search(string jql)
         {
-            return string.Format("/rest/api/2/search?jql={0}", jql);
+            return string.Format("rest/api/2/search?jql={0}", jql);
         }
 
         public static string ResolvedIssues(string fromDate, string endDate)
