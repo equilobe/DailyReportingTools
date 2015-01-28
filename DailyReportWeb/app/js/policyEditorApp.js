dbb8e3986cb55b79370bf60f87470e603757ade7
Vlad Ichimescu <vladichimescu@gmail.com>
2015-01-28 17:58:40 +0200

    Fixed report time validation due to angular rendering in DOM

--- DailyReportWeb/app/js/policyEditorApp.js
+++ DailyReportWeb/app/js/policyEditorApp.js
@@ -1,6 +1,28 @@
 ﻿angular.module("policyEditorApp", [])
     .controller("policyListPage", ['$scope', '$http', function ($scope, $http) {
-        $http.get("/api/policy").success(function(list){
-            $scope.policyList = list;
-        })        
-    }]);
+    	$http.get("/api/policy").success(function (list) {
+    		$scope.policyList = list;
+    	})
+    }])
+	.directive('onLastRepeat', function () {
+		return function (scope, element, attrs) {
+			if (scope.$last) setTimeout(function () {
+				$("input").on("keyup", function () {
+					var selectedInput = $(this);
+
+					if (selectedInput.hasClass("ng-invalid")) {
+						selectedInput.parent().addClass("has-error")
+							.find('.reportTime-notification').show();
+					}
+					else {
+						selectedInput.parent().removeClass("has-error")
+							.find('.reportTime-notification').hide();;
+					}
+				});
+
+				$(".reportTime-notification").on("click", function () {
+					$(this).prev().focus();
+				});
+			}, 1);
+		};
+	});
