d8d495badf19421c623e54e79c5cc9badefb31e9
Vlad Ichimescu <vladichimescu@gmail.com>
2015-01-29 13:11:10 +0200

    Switch input report time validation to be based on angular and css [removed js]

--- DailyReportWeb/app/js/policyEditorApp.js
+++ DailyReportWeb/app/js/policyEditorApp.js
@@ -3,26 +3,4 @@
     	$http.get("/api/policy").success(function (list) {
     		$scope.policyList = list;
     	})
-    }])
-	.directive('onLastRepeat', function () {
-		return function (scope, element, attrs) {
-			if (scope.$last) setTimeout(function () {
-				$("input").on("keyup", function () {
-					var selectedInput = $(this);
-
-					if (selectedInput.hasClass("ng-invalid")) {
-						selectedInput.parent().addClass("has-error")
-							.find('.reportTime-notification').show();
-					}
-					else {
-						selectedInput.parent().removeClass("has-error")
-							.find('.reportTime-notification').hide();;
-					}
-				});
-
-				$(".reportTime-notification").on("click", function () {
-					$(this).prev().focus();
-				});
-			}, 1);
-		};
-	});
+    }]);
