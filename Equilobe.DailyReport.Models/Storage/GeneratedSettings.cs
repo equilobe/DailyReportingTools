c7546de697417d51ebe9319aa910b5bdd6f4bffd
Sebastian Dumitrascu <seby_dum@yahoo.com>
2015-02-13 15:12:05 +0200

    implemented classes for database tables regarding generated properties

---
+++ Equilobe.DailyReport.Models/Storage/GeneratedSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equilobe.DailyReport.Models.Storage
+{
+    public class GeneratedSettings
+    {
+        public long ReportSettingsId { get; set; }
+        public DateTime LastReportSentDate { get; set; }
+        public DateTime LastDraftSentDate { get; set; }
+        public bool WasResetToDefaultToday { get; set; }
+        public bool IsFinalDraftConfirmed { get; set; }
+        public bool WasForcedByLead { get; set; }
+        public List<IndividualDraftSettings> IndividualDrafts { get; set; }
+    }
+}
