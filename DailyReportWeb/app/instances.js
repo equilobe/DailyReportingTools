1dc0ab9b607cdc7d3c150f5753fa4d23db50917d
Vlad Ichimescu <vladichimescu@gmail.com>
2015-03-17 17:15:54 +0200

    DRT-177 - removed hashtag from spa routing

--- DailyReportWeb/app/instances.js
+++ DailyReportWeb/app/instances.js
@@ -2,7 +2,7 @@
 
 angular.module('app')
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/home/list', { templateUrl: 'app/instances.html', controller: 'InstanceCtrl' });
+        $routeProvider.when('/app/list', { templateUrl: 'app/instances.html', controller: 'InstanceCtrl' });
     }])
     .controller("InstanceCtrl", ['$scope', '$http', function ($scope, $http) {
         $scope.status = "loading";
