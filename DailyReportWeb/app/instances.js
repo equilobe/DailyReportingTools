538a93376523f200b65c847456da9406aaed2ce6
Vlad Ichimescu <vladichimescu@gmail.com>
2015-03-16 20:03:09 -0700

    DRT-177 - updated angular core - added angular route - added SPA routing - added angular controllers/views - removed unneeded views

---
+++ DailyReportWeb/app/instances.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+angular.module('app')
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/home/list', { templateUrl: 'app/instances.html', controller: 'InstanceCtrl' });
+    }])
+    .controller("InstanceCtrl", ['$scope', '$http', function ($scope, $http) {
+        $scope.status = "loading";
+
+        $http.get("/api/project").success(function (list) {
+            $scope.instances = list;
+
+            $scope.status = "loaded";
+        });
+    }]);
