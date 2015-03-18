2ea0d4a4fa60b155b4d0a3b8574fb94d7a343010
bindea-mihai <bindea.mihai@yahoo.com>
2015-03-18 17:55:31 +0200

    DRT-188 User Management - refactored logic that handles sending the account confirmation mail the a newly registered user

--- DailyReportWeb/App_Start/IdentityConfig.cs
+++ DailyReportWeb/App_Start/IdentityConfig.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Equilobe.DailyReport.Models.Storage;
 using Equilobe.DailyReport.DAL;
+using System.Net.Mail;
 
 namespace DailyReportWeb
 {
@@ -56,32 +57,12 @@ namespace DailyReportWeb
 	{
 		public Task SendAsync(IdentityMessage message)
 		{
-			#region credentials
-			// Credentials:
-			string sendGridUserName = "bindeamih";
-			string sentFrom = "bpro-no-reply@equilobe.com";
-			string sendGridPassword = "WorldOfTanks";
-			#endregion
-			// Configure the client
-			var client = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
-
-			client.Port = 587;
-			client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-			client.UseDefaultCredentials = false;
-
-			// Create the credentials:
-			System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(sendGridUserName, sendGridPassword);
-
-			client.EnableSsl = true;
-			client.Credentials = credentials;
-
-			// Create the message:
-			var mail = new System.Net.Mail.MailMessage(sentFrom, message.Destination);
-
+            var client = new SmtpClient() { EnableSsl = true };
+            var credential = (System.Net.NetworkCredential)client.Credentials;
+			var mail = new MailMessage(credential.UserName, message.Destination);
 			mail.Subject = message.Subject;
 			mail.Body = message.Body;
 
-			// Send:
 			return client.SendMailAsync(mail);
 		}
 	}
