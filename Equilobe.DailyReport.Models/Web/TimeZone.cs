5372bc89c7f6ee3eab19fd7f6d10b61e91520ffc
bindea-mihai <bindea.mihai@yahoo.com>
2015-03-24 17:11:33 +0200

    DRT-141 Authorization - updated the timeZone class - added new class for all system time zones

--- Equilobe.DailyReport.Models/Web/TimeZone.cs
+++ Equilobe.DailyReport.Models/Web/TimeZone.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace Equilobe.DailyReport.Models.Web
 {
+    public class TimeZones
+    {
+        public List<TimeZone> TimeZoneList { get; set; }
+        public string SuggestedTimeZone { get; set; }
+    }
+
     public class TimeZone
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public double UtcOffset { get; set; }
     }
+
+
 }
