469bea264294515ec154e4d946b27011b1b88612
bindea-mihai <bindea.mihai@yahoo.com>
2015-03-24 17:12:33 +0200

    DRT-141 Authorization - added interface for the time zone service

---
+++ Equilobe.DailyReport.Models/Interfaces/ITimeZoneService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equilobe.DailyReport.Models.Interfaces
+{
+    public interface ITimeZoneService : IService
+    {
+        /// <summary>
+        /// Maps the IANA time zone with SystemTimeZoneId. Retrns an empty string if there is no equivalent
+        /// </summary>
+        /// <param name="ianaTimeZone"></param>
+        /// <returns>SystemTimeZoneId</returns>
+        string GetWindowsTimeZoneIdByIanaTimeZone(string ianaTimeZone);
+    }
+}
