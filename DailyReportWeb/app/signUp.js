68c73bad37e3a5984300c3aaf87411e0de7129dc
bindea-mihai <bindea.mihai@yahoo.com>
2015-03-24 17:19:46 +0200

    DRT-141 Authorization - added logic to suggest a time zone based on the users location

--- DailyReportWeb/app/signUp.js
+++ DailyReportWeb/app/signUp.js
@@ -8,12 +8,39 @@ angular.module('app')
         });
     }])
     .controller("SignUpCtrl", ['$scope', '$http', function ($scope, $http) {
-        $http.get("/api/timezone")
-            .success(function (list) {
-                $scope.timeZoneList = list;
-            })
-            .error(function () {
-                $scope.status = "error";
+        
+
+        $http.get("http://ipinfo.io/json")
+             .then(function (result) {
+                 var ip = result.data.ip;
+                 return ip;
+             })
+             .then(function (ip) {
+                 return $http.get("http://freegeoip.net/json/" + ip);
+             })
+            .then(function (result) {
+                if (result.data.time_zone != undefined) {
+                    //this is a hack so that the time zone id will not be mistaken as being part of the URL
+                    var timeZoneId = result.data.time_zone.replace(/\//g, '-'); 
+                    $http.get("/api/timezone/" + timeZoneId)
+                    .success(function (response) {
+                        $scope.timeZoneList = response.timeZoneList;
+                        $scope.signUpForm.timeZone = response.suggestedTimeZone;
+                    });
+                }
+                else {
+                    $http.get("/api/timezone")
+                         .success(function (list) {
+                             $scope.timeZoneList = list;
+                             var timeZoneOffset = new Date().getTimezoneOffset(); //represents the difference in minutes between UTC and the user's time zone (eg. for +2 a time zone it will be -120)
+                             var closestTimeZone = $.grep($scope.timeZoneList, function (element) { return element.utcOffset == -timeZoneOffset })[0];
+                             if (closestTimeZone != undefined)
+                                 $scope.signUpForm.timeZone = closestTimeZone.id;
+                         })
+                         .error(function () {
+                             $scope.status = "error";
+                         });
+                }
             });
 
         $scope.signUp = function ($scope) {
