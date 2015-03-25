77195d7e6094bc7432c5664a690659691ca820d8
Vlad Ichimescu <vladichimescu@gmail.com>
2015-03-25 17:02:16 +0200

    DRT-178 - moved method concerning plugin to PluginController - enabled plugin auth to Daily Report webapp - extracted main menu as partial view - removed jira route config as it's no longer needed - removed JwtAuthenticationAttribute as it's been refactored - set atlassian connect resources to load only when using as plugin

---
+++ DailyReportWeb/Controllers/PluginController.cs
@@ -0,0 +1,105 @@
+﻿using Equilobe.DailyReport.Models.Interfaces;
+using Equilobe.DailyReport.Models.Storage;
+using Equilobe.DailyReport.Models.TaskScheduling;
+using Newtonsoft.Json;
+using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace DailyReportWeb.Controllers
+{
+    public class PluginController : Controller
+    {
+        public ITaskSchedulerService TaskSchedulerService { get; set; }
+        public IDataService DataService { get; set; }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var descriptor = new
+            {
+                name = "Daily Report Tool",
+                description = "A Connect add-on that makes JIRA info available to Daily Report Tool",
+                key = ConfigurationManager.AppSettings["addonKey"],
+                baseUrl = UrlExtensions.GetHostUrl(Request.Url.OriginalString),
+                vendor = new
+                {
+                    name = "Equilobe Software",
+                    url = "http://equilobe.com/"
+                },
+                links = new
+                {
+                    self = UrlExtensions.GetHostUrl(Request.Url.OriginalString) + "/howto",
+                    documentation = UrlExtensions.GetHostUrl(Request.Url.OriginalString) + "/docs"
+                },
+                apiVersion = 1,
+                authentication = new
+                {
+                    type = "jwt"
+                },
+                lifecycle = new
+                {
+                    installed = "/plugin/installed",
+                    uninstalled = "/plugin/uninstalled"
+                },
+                scopes = new[]
+                {
+                    "PROJECT_ADMIN"
+                },
+                modules = new
+                {
+                    generalPages = new[] 
+                    { 
+                        new 
+                        {
+                            name = new 
+                            {
+                                value = "DRT"
+                            },
+                            url = "/",
+                            key = "app",
+                            location = "system.top.navigation.bar",
+                            conditions = new[]
+                            {
+                                new
+                                {
+                                    condition = "user_is_admin"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            return Json(descriptor, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Installed()
+        {
+            var bodyText = new System.IO.StreamReader(Request.InputStream).ReadToEnd();
+            var instanceData = JsonConvert.DeserializeObject<InstalledInstance>(bodyText);
+            DataService.SaveInstance(instanceData);
+
+            return Content(String.Empty);
+        }
+
+        [HttpPost]
+        public ActionResult Uninstalled()
+        {
+            var bodyText = new System.IO.StreamReader(Request.InputStream).ReadToEnd();
+            var baseUrl = JsonConvert.DeserializeObject<InstalledInstance>(bodyText).BaseUrl;
+
+            var projectKeys = DataService.GetUniqueProjectsKey(baseUrl);
+
+            TaskSchedulerService.DeleteMultipleTasks(new ProjectListContext
+            {
+                UniqueProjectKeys = projectKeys
+            });
+            DataService.DeleteInstance(baseUrl);
+
+            return Content(String.Empty);
+        }
+    }
+}
---
+++ DailyReportWeb/Helpers/AuthHelpers.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Security;
+using Equilobe.DailyReport.Models.Web;
+using Equilobe.DailyReport.Models.Interfaces;
+using Equilobe.DailyReport.SL;
+using System.Collections.Specialized;
+using Equilobe.DailyReport.JWT;
+
+namespace DailyReportWeb
+{
+    public static class AuthHelpers
+    {
+        private const int CookieVersion = 1;
+
+        public static void PluginAuth()
+        {
+            var request = new HttpRequestWrapper(HttpContext.Current.Request);
+            if (!IsPlugin(request))
+                return;
+
+            var userName = request.QueryString["user_id"];
+            var baseUrl = request.QueryString["xdm_e"] + request.QueryString["cp"];
+            SetAuthCookie(userName, baseUrl);
+        }
+
+        public static void SetAuthCookie(string userName, string baseUrl, bool rememberMe = true)
+        {
+            SetAuthCookie(userName, baseUrl, GetExpirationDate(rememberMe));
+        }
+
+        private static void SetAuthCookie(string userName, string baseUrl, DateTime expirationDate)
+        {
+            var cookie = new HttpCookie(FormsAuthentication.FormsCookieName)
+            {
+                Value = GetEncryptedAuthTicket(userName, baseUrl, expirationDate),
+                Expires = expirationDate
+            };
+
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+
+        private static DateTime GetExpirationDate(bool rememberMe)
+        {
+            if (rememberMe)
+                return DateTime.Now.Add(new TimeSpan(14, 0, 0, 0));
+
+            return DateTime.Now.Add(FormsAuthentication.Timeout);
+        }
+
+        private static string GetEncryptedAuthTicket(string userName, string baseUrl, DateTime expirationDate)
+        {
+            var ticket = new FormsAuthenticationTicket(
+                                version: CookieVersion,
+                                name: userName,
+                                issueDate: DateTime.Now,
+                                expiration: expirationDate,
+                                isPersistent: true,
+                                userData: baseUrl);
+
+            return FormsAuthentication.Encrypt(ticket);
+        }
+
+        private static FormsAuthenticationTicket GetAuthTicket()
+        {
+            var request = HttpContext.Current.Request;
+            var authCookie = request.Cookies.Get(FormsAuthentication.FormsCookieName);
+            try
+            {
+                return FormsAuthentication.Decrypt(authCookie.Value);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static string GetBaseUrl(this IPrincipal webPrincipal)
+        {
+            var authTicket = GetAuthTicket();
+
+            if (authTicket != null)
+                return authTicket.UserData;
+            return null;
+        }
+
+        public static string GetUsername(this IPrincipal webPrincipal)
+        {
+            return webPrincipal.Identity.Name;
+        }
+
+        public static bool IsPlugin(this IPrincipal webPrincipal)
+        {
+            return !string.IsNullOrEmpty(webPrincipal.GetBaseUrl());
+        }
+
+        public static bool IsPlugin(HttpRequestBase request)
+        {
+            try
+            {
+                var requestToken = request.QueryString["jwt"];
+                if (string.IsNullOrEmpty(requestToken))
+                    return false;
+
+                var baseUrl =  request.QueryString["xdm_e"] + request.QueryString["cp"];
+                if (string.IsNullOrEmpty(baseUrl))
+                    return false;
+
+                var sharedSecret = new DataService().GetSharedSecret(baseUrl);
+                var token = new EncodedJwtToken(sharedSecret, requestToken).Decode();
+                token.ValidateToken(request);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
