75ffcc7714a60288b13b4450e0ab5874d52fe46d
Sebastian Dumitrascu <seby_dum@yahoo.com>
2015-03-26 15:17:09 +0200

    DRT-202 - improved instances.html and instances.js to allow user to add new instance;

--- DailyReportWeb/app/instances.js
+++ DailyReportWeb/app/instances.js
@@ -18,4 +18,24 @@ angular.module('app')
             .finally(function () {
                 $scope.status = "loaded";
             });
+
+        $scope.addNewInstance = function ($scope) {
+            $scope.addingInstance = true;
+        };
+
+        $scope.addInstance = function ($scope) {
+            $scope.status = 'saving';
+            $scope.newInstanceForm.$setPristine();
+
+            $http.post("/api/instances/" + $scope.newInstanceForm
+                ).success(function (list) {
+                    $scope.instances = list;
+                    $scope.addingInstance = false;
+                    $scope.status = "success";
+                    console.log("success");
+                }).error(function () {
+                    $scope.status = "error";
+                    console.log("error");
+                });
+        };
     }]);
