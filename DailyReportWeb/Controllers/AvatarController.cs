c87ba80de3add42d3b335d6076962f4d9b35f57a
Sebastian Dumitrascu <seby_dum@yahoo.com>
2015-04-09 19:36:32 +0300

    DRT-146 - added avatar controller;

---
+++ DailyReportWeb/Controllers/AvatarController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DailyReportWeb.Controllers
+{
+    public class AvatarController : Controller
+    {
+        // GET: Avatar
+        public ActionResult Avatar(string key)
+        {
+
+            return View();
+        }
+    }
+}
