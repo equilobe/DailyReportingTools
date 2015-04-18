7b4398e5d0569b585b899453136f23a2b45cb7ea
Vlad Ichimescu <vladichimescu@gmail.com>
2015-04-18 18:08:17 +0300

    DRT-219 - added eula page - updated header - updated footer - refactored how page is positioned relative to main menu

---
+++ DailyReportWeb/app/eula.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+angular.module('app')
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/app/eula', {
+            templateUrl: 'app/eula.html',
+            controller: 'EulaController'
+        });
+    }])
+    .controller("EulaController", ["$scope", "$http", function ($scope, $http) {
+        $("body").attr("data-page", "eula");
+        $scope.$parent.child = $scope;
+    }]);
