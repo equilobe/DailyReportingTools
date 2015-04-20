32b053aa0d52fd4b3997567f94a2d5315001d73b
Vlad Ichimescu <vladichimescu@gmail.com>
2015-04-20 16:39:45 +0300

    DRT-219 - added 'how it works' page - added sample content to page - updated link in header/footer to 'how it works' page

---
+++ DailyReportWeb/app/howItWorks.test.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+describe('app module', function () {
+    beforeEach(module('app'));
+
+    describe('HowItWorksController controller', function () {
+        it('should ....', inject(function ($controller) {
+            var controller = $controller('HowItWorksController');
+            expect(controller).toBeDefined();
+        }));
+    });
+});
