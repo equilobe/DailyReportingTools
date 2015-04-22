85d0e42f17c7ffede0292579cbbb83c1aa5a7a62
Sebastian Dumitrascu <seby_dum@yahoo.com>
2015-04-22 15:55:57 +0300

    DRT-169 - round sprint date to start of the day when calculating sprint time spent

--- JiraReporter/Services/AuthorLoader.cs
+++ JiraReporter/Services/AuthorLoader.cs
@@ -111,7 +111,7 @@ namespace JiraReporter.Services
         {
             _currentAuthor.Issues = GetAuthorsTimesheetIssues(_options.FromDate, _options.ToDate);
             if (_sprint != null)
-                _currentAuthor.SprintIssues = GetAuthorsTimesheetIssues(_sprint.StartDate.ToOriginalTimeZone(_context.OffsetFromUtc), _options.ToDate);
+                _currentAuthor.SprintIssues = GetAuthorsTimesheetIssues(_sprint.StartDate.ToOriginalTimeZone(_context.OffsetFromUtc).Date, _options.ToDate);
             _currentAuthor.MonthIssues = GetAuthorsTimesheetIssues(_options.FromDate.StartOfMonth(), _options.ToDate);
         }
 
