e3a65bf54c7cd8df1b6b1d2b9dec7228435928ae
Sebastian Dumitrascu <seby_dum@yahoo.com>
2015-08-17 19:56:35 +0300

    DRT-164 - added ipnLogs.xml to log the IPN signals - created PayPalLog class that is serialized into ipnLogs.xml

---
+++ Equilobe.DailyReport.Models/Paypal/PayPalLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equilobe.DailyReport.Models.Paypal
+{
+    public class PayPalLog
+    {
+        public PayPalCheckoutInfo PayPalInfo { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
