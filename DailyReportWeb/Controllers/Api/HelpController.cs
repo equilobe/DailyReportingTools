a8ecf10b186ebd9442803044c57de7cd5c2201dd
Sebastian Dumitrascu <seby_dum@yahoo.com>
2015-09-23 16:19:22 +0300

    -added help page

---
+++ DailyReportWeb/Controllers/Api/HelpController.cs
@@ -0,0 +1,19 @@
+﻿using Equilobe.DailyReport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DailyReportWeb.Controllers.Api
+{
+    [Authorize]
+    public class HelpController : ApiController
+    {
+        public SimpleResult Get()
+        {
+            return SimpleResult.Success("");
+        }
+    }
+}
---
+++ DailyReportWeb/app/help.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+angular.module('app')
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/app/help', {
+            templateUrl: 'app/help.html',
+            controller: 'HelpController'
+        });
+    }])
+    .controller("HelpController", ["$scope", "$http", "$location", function ($scope, $http, $location) {
+        $("body").attr("data-page", "help");
+        $scope.$parent.child = $scope;
+        $scope.stauts = "";
+
+        $http.get("api/help")
+            .finally(function (result) {
+                $scope.status = result.message;
+            });
+
+    }]);
