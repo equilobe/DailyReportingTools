dd36c987347a65168b1b4c6020e760efe22dbb64
Sebastian Dumitrascu <seby_dum@yahoo.com>
2016-05-26 15:27:37 +0300

    DRT-246 - updated AuthorLoader

--- JiraReporter/Services/AuthorLoader.cs
+++ JiraReporter/Services/AuthorLoader.cs
@@ -137,7 +137,7 @@ namespace JiraReporter.Services
             _currentAuthor.Issues = GetAuthorsTimesheetIssues(_options.FromDate, _options.ToDate);
             if (_sprint != null)
                 _currentAuthor.SprintIssues = GetAuthorsTimesheetIssues(_sprint.StartDate.ToOriginalTimeZone(_context.OffsetFromUtc).Value.Date, _options.ToDate);
-            _currentAuthor.MonthIssues = GetAuthorsTimesheetIssues(_startOfMonth, _startOfMonth.EndOfMonth());
+            _currentAuthor.MonthIssues = GetAuthorsTimesheetIssues(_startOfMonth, _startOfMonth.EndOfMonth().AddDays(1).AddMinutes(-1));
         }
 
         private List<IssueDetailed> GetAuthorsTimesheetIssues(DateTime fromDate, DateTime toDate)
