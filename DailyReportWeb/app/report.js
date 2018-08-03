6847b2b6895d91775a7c892e73c1acc6be12cfbc
Georgian Tanase <georgian.tanase@equilobe.com>
2018-08-03 15:08:35 +0300

    DD-3 - create a new page in the instance, for seeing the dashboard report

---
+++ DailyReportWeb/app/report.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+angular.module('app')
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/app/instances/:instanceId/report', {
+            templateUrl: 'app/report.html',
+            controller: 'ReportController'
+        });
+    }])
+    .controller("ReportController", ["$scope", "$http", '$routeParams', function ($scope, $http, $routeParams) {
+        $("body").attr("data-page", "report");
+        $scope.$parent.child = $scope;
+        $scope.status = "loading";
+
+        $http.get("/api/report/" + $routeParams.instanceId)
+            .success(function (data) {
+                $scope.data = data;
+            })
+            .finally(function () {
+                $scope.status = "loaded";
+            });
+    }])
