c7d954e0f686739c6f246139bf8d8b99e99de779
Georgian Tanase <georgian.tanase@equilobe.com>
2018-08-07 17:05:58 +0300

    DD-2 - get the users from all the projects from a specific instance

--- DailyReportWeb/Controllers/Api/ReportController.cs
+++ DailyReportWeb/Controllers/Api/ReportController.cs
@@ -1,18 +1,21 @@
-﻿using System.Web.Http;
+﻿using Equilobe.DailyReport.Models.Interfaces;
+using System.Web.Http;
 
 namespace DailyReportWeb.Controllers.Api
 {
     [Authorize]
     public class ReportController : ApiController
     {
+        public IReportService ReportService { get; set; }
+
         public object Get(long id)
         {
             return null;
         }
 
-        public void Post([FromBody] long id)
+        public void Post(long id)
         {
-            var a = 5;
+            ReportService.UpdateDashboardData(id);
         }
     }
 }
---
+++ Equilobe.DailyReport.SL/ReportService.cs
@@ -0,0 +1,60 @@
+﻿using Equilobe.DailyReport.DAL;
+using Equilobe.DailyReport.Models.Interfaces;
+using Equilobe.DailyReport.Models.Jira;
+using Equilobe.DailyReport.Models.ReportFrame;
+using Equilobe.DailyReport.Models.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Equilobe.DailyReport.SL
+{
+    public class ReportService : IReportService
+    {
+        public IJiraService JiraService { get; set; }
+
+        #region IJiraService Implementation
+        public void UpdateDashboardData(long instanceId)
+        {
+            using (var db = new ReportsDb())
+            {
+                var users = GetUsersFromInstance(db, instanceId);
+
+                //TODO - add in DB 
+            }
+        }
+        #endregion
+
+        #region Helpers
+        private List<JiraUser> GetUsersFromInstance(ReportsDb db, long instanceId)
+        {
+            var basicSettings = db.BasicSettings.Where(p => p.InstalledInstanceId == instanceId).Select(p => new { p.ProjectId, p.InstalledInstance }).ToList();
+            var jiraContext = new JiraRequestContext();
+            var users = new List<JiraUser>();
+
+            foreach (var setting in basicSettings)
+            {
+                try
+                {
+                    setting.InstalledInstance.CopyPropertiesOnObjects(jiraContext);
+                    var project = JiraService.GetProject(jiraContext, setting.ProjectId);
+                    var projectUsers = JiraService.GetUsers(jiraContext, project.Key);
+
+                    users.AddRange(projectUsers);
+                }
+                catch (Exception ex) { }
+            }
+
+            return RemoveUserDuplicates(users);
+        }
+
+        private List<JiraUser> RemoveUserDuplicates(List<JiraUser> users)
+        {
+            return users
+                .GroupBy(p => p.emailAddress)
+                .Select(p => p.First())
+                .ToList();
+        }
+        #endregion
+    }
+}
