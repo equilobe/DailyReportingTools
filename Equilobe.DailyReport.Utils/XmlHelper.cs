18ceb7151c8065bfcc8ae82b287ae7d196cc61d5
Georgian Tanase <georgian.tanase@equilobe.com>
2018-09-10 11:00:26 +0300

    DD-16 - refactor

---
+++ Equilobe.DailyReport.Utils/XmlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Equilobe.DailyReport.Utils
+{
+    public static class XmlHelper
+    {
+        public static T DeserializeXml<T>(string data)
+            where T : new()
+        {
+            var deserialized = new T();
+
+            Deserialization.XmlDeserialize<T>(data)
+                .CopyPropertiesOnObjects(deserialized);
+
+            return deserialized;
+        }
+    }
+}
